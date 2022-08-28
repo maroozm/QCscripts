f0e92644be6d036f1e0f8bc992f89184f6b0b166
maroozm <salman.khurshid.malik@cern.ch>
2022-08-28 14:42:46 +0530

    ctf files in text

---
+++ ctffiles.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+function msg() {
+  local text="$1"
+  local div_width="80"
+  printf "%${div_width}s\n" ' ' | tr ' ' -
+  printf "%s\n" "$text"
+}
+
+function alienv() {
+  if command -v alien.py &>/dev/null; then
+    echo ""
+  else
+    msg "[WARN]: Make sure alienv environment is loaded"
+    exit 1
+  fi
+}
+
+alienv
+
+msg "Please Provide the grid path"
+read -p " " -r path
+
+msg "Name the file: {"Enter" for default}"
+read -p " " -r answer
+
+if [ "$answer" == "" ];
+then
+  declare name=$(echo "$path" | awk -F "/" '{print $6}')
+else
+  declare name=$answer
+fi
+
+
+msg "Creating file for you"
+
+alien_ls $path > $name.txt
+sed -i "s+^+alien://$path/+" "$name.txt"
+
+msg "DONE"
